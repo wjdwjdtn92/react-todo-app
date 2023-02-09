f4abd5e2874ee2a5aefa6a59527334bedbfcac9b
Jung Su <wjdwjdtn92@naver.com>
2023-02-09 21:51:43 +0900

    🔨 Add header and next prev button

---
+++ src/Layout/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { GrNext, GrPrevious } from 'react-icons/gr';
+import styled, { css } from 'styled-components';
+
+const Container = styled.div`
+  background-color: #79e6bf;
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-radius: 20px;
+`;
+
+const buttonStyles = css`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const NextButton = styled(GrNext)`
+  ${buttonStyles}
+`;
+
+const PrevButton = styled(GrPrevious)`
+  ${buttonStyles}
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  font-size: 18px;
+  flex: 1 1 0;
+`;
+
+interface IHeader {
+  date: string;
+  handleNextClick: () => void;
+  handlePrevClick: () => void;
+}
+
+function Header({ date, handleNextClick, handlePrevClick }: IHeader) {
+  const today = new Date().toLocaleDateString().slice(0, -1);
+
+  return (
+    <Container>
+      <PrevButton onClick={handlePrevClick} />
+      <Title>todo ({date})</Title>
+      <NextButton onClick={handleNextClick} />
+    </Container>
+  );
+}
+
+export default Header;
