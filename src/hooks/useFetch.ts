0ba7eb2b5625741128a69e646eeedb30c46169ae
Jung Su <wjdwjdtn92@naver.com>
2023-02-09 22:03:13 +0900

    ♻️ 다른 method 요청도 가능하도록 수정

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,16 +3,25 @@ import { ITodoItem } from '../components/TodoList/TodoItem';
 
 interface IProps {
   url: string;
+  method?: string;
+  sendData?: {};
 }
 
-function useFetch({ url }: IProps) {
+function useFetch({ url, method, sendData }: IProps) {
   const [data, setData] = useState<Array<ITodoItem>>([]);
   const [error, setError] = useState<string>('');
 
   useEffect(() => {
     const abortCont = new AbortController();
 
-    fetch(url, { signal: abortCont.signal })
+    fetch(url, {
+      signal: abortCont.signal,
+      method: method && 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      ...(sendData ?? { body: JSON.stringify(sendData) }),
+    })
       .then(res => {
         if (!res.ok) {
           // error coming back from server
